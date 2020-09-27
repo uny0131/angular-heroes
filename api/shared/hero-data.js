5025261484cbb8febc637e1c913d9959e3b752dd
uny0131 <rsr78793@nifty.com>
2020-09-27 19:11:37 +0900

    test

--- api/shared/hero-data.js
+++ api/shared/hero-data.js
@@ -9,7 +9,9 @@ const data = {
         { id: 17, name: 'Dynama' },
         { id: 18, name: 'Dr IQ' },
         { id: 19, name: 'Magma' },
-        { id: 20, name: 'Tornado' }
+        { id: 20, name: 'Tornado' },
+        { id: 21, name: 'aiueo'},
+        { id: 22, name: 'kaki'}
     ]
 };
 /*
